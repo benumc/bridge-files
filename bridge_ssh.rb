3c20120fd9ce2de3491fa087b2782267f9f395eb
benumc <ben@paramountautomation.com>
2022-05-19 06:13:49 -0700

    Update bridge_ssh.rb

--- bridge_ssh.rb
+++ bridge_ssh.rb
@@ -3,12 +3,12 @@ $0 = 'bridge_ssh'
 #Process.daemon
 $udp_socket = UDPSocket.new
 $udp_socket.bind('127.0.0.1', 0)
-puts("Existing listeners: #{`lsof -nP -i4TCP:25803 | grep LISTEN`}")
+puts("Existing listeners:\t #{`lsof -nP -i4TCP:25803 | grep LISTEN`}")
 $tcp_server = TCPServer.new('127.0.0.1',25803)
-puts('listening on 25803')
+puts('listening on:\t 25803')
 $local_ip = Socket.ip_address_list.to_s[/ ((?!127)\d\d?\d?\.[0-9]+\.[0-9]+\.[0-9]+)/,1]
-puts("local ip: #{$local_ip}")
-puts("bridge file: #{`ls /tmp/bridge_ssh`}")
+puts("local ip:\t #{$local_ip}")
+puts("bridge file:\n #{`ls /tmp/bridge_ssh`}")
 exit
 
 def udp_send(dat)
